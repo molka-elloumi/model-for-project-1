2d4af0d1e82d7caa1afc2170885140a60b967778
Ahlemj-bit <ahlemj023@gmail.com>
2025-03-15 22:04:07 +0000

    Update ML code.py

--- ML code.py
+++ ML code.py
@@ -16,3 +16,58 @@ from imblearn.under_sampling import RandomUnderSampler
 from sklearn.utils.class_weight import compute_class_weight
 from sklearn.model_selection import train_test_split
 df = pd.read_csv("heart_failure_clinical_records_dataset.csv")
+
+df
+
+"""##Business understanding:
+Enhancing clinical decision-making by providing accurate, interpretable, and efficient heart failure risk predictions to support physicians in patient care.
+
+#Data understanding:
+Thirteen (13) clinical features:
+
+- age: age of the patient (years)
+- anaemia: decrease of red blood cells or hemoglobin (boolean)
+- creatinine phosphokinase  (CPK): level of the CPK enzyme in the blood (mcg/L)
+- diabetes: if the patient has diabetes (boolean)
+- ejection fraction: percentage of blood leaving the heart at each contraction  (percentage)
+- high blood pressure: if the patient has hypertension (boolean)
+- platelets: platelets in the blood (kiloplatelets/mL)df
+
+"""##Business understanding:
+Enhancing clinical decision-making by providing accurate, interpretable, and efficient heart failure risk predictions to support physicians in patient care.
+
+#Data understanding:
+Thirteen (13) clinical features:
+
+- age: age of the patient (years)
+- anaemia: decrease of red blood cells or hemoglobin (boolean)
+- creatinine phosphokinase  (CPK): level of the CPK enzyme in the blood (mcg/L)
+- diabetes: if the patient has diabetes (boolean)
+- ejection fraction: percentage of blood leaving the heart at each contraction  (percentage)
+- high blood pressure: if the patient has hypertension (boolean)
+- platelets: platelets in the blood (kiloplatelets/mL)
+- sex: woman or man (binary)
+- serum creatinine: level of serum creatinine in the blood (mg/dL)
+- serum sodium: level of serum sodium in the blood (mEq/L)
+- smoking: if the patient smokes or not (boolean)
+- time: follow-up period (days)
+- [target] death event: if the patient died during the follow-up period (boolean)
+
+#Explatory data analysis:
+"""
+
+# Handling Missing Values
+
+
+- sex: woman or man (binary)
+- serum creatinine: level of serum creatinine in the blood (mg/dL)
+- serum sodium: level of serum sodium in the blood (mEq/L)
+- smoking: if the patient smokes or not (boolean)
+- time: follow-up period (days)
+- [target] death event: if the patient died during the follow-up period (boolean)
+
+#Explatory data analysis:
+"""
+
+# Handling Missing Values
+
